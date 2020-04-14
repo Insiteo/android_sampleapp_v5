5de9cf387bdab744782cd1de8a8bec4c9dccb46f
cmaquaire <cmaquaire@insiteo.com>
2020-04-14 20:28:30 +0200

    Mise à jour de l'application et de la librairie.

---
+++ app/src/main/java/com/insiteo/samplappv5/MainActivity.java
@@ -0,0 +1,111 @@
+package com.insiteo.samplappv5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.insiteo.sdk_v5.INSMapFragment;
+import com.insiteo.sdk_v5.mapModule.MapManager;
+import com.insiteo.sdk_v5.mapModule.UnityMapHandlerListener;
+import com.insiteo.sdk_v5.mapModule.entities.Building;
+import com.insiteo.sdk_v5.mapModule.entities.Floor;
+import com.insiteo.sdk_v5.mapModule.entities.Site;
+import com.insiteo.sdk_v5.mapModule.entities.Space;
+import com.insiteo.sdk_v5.mapModule.entities.SubArea;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements UnityMapHandlerListener {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FrameLayout fl = findViewById(R.id.fl_forUnity);
+        INSMapFragment frag = MapManager.getInstance().initDisplayComponent();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.add(fl.getId(), frag).commit();
+
+        button = findViewById(R.id.fl_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "BOOM!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        MapManager.getInstance().registerHandler(this);
+        MapManager.getInstance().changeDisplayMode(0);
+    }
+
+    @Override
+    public void onStartLocationEventListener() {
+
+    }
+
+    @Override
+    public void onStopLocationEventListener() {
+
+    }
+
+    @Override
+    public void onChangeSiteEventListener(String s) {
+
+    }
+
+    @Override
+    public void onSiteDisplayed() {
+
+    }
+
+    @Override
+    public void onSiteLoaded() {
+
+    }
+
+    @Override
+    public void onDisplayModeChanged() {
+
+    }
+
+    @Override
+    public void onSiteReceived(List<Site> list) {
+
+    }
+
+    @Override
+    public void onBuildingsReceived(List<Building> list) {
+
+    }
+
+    @Override
+    public void onFloorsReceived(List<Floor> list) {
+
+    }
+
+    @Override
+    public void onSpacesReceived(List<Space> list) {
+
+    }
+
+    @Override
+    public void onDeskReceived(List<SubArea> list) {
+
+    }
+
+    @Override
+    public void onAssetTypeReceived(String s) {
+
+    }
+
+    @Override
+    public void onSpaceReceived(Space space) {
+
+    }
+}
